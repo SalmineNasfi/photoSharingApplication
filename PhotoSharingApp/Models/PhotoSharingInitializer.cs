4fa7c53ab5d2b6b9d1f2f70fa18dc2f95c372aaa
SalmineNasfi <nasfi.salmine@gmail.com>
2018-05-18 17:51:41 +0200

    modification

--- PhotoSharingApp/Models/PhotoSharingInitializer.cs
+++ PhotoSharingApp/Models/PhotoSharingInitializer.cs
@@ -1,19 +1,42 @@
-﻿using System;
+﻿using PhotoSharingApp.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.Entity;
 using System.IO;
+using System.Linq;
+using System.Web;
 
-namespace PhotoSharingApp.Models
+namespace PhotoSharingApp.Model
 {
-    public class PhotoSharingInitializer : DropCreateDatabaseAlways
-<PhotoSharingContext>
+    public class PhotoSharingInitializer : DropCreateDatabaseAlways<PhotoSharingContext>
     {
-    }
-}
+        protected override void Seed(PhotoSharingContext context)
+        {
+            List<Photo> photos = new List<Photo>();
+            Photo photo = new Photo();
+            photo.Title = "Test Photo";
+            photo.Description = "test";
+            photo.Owner = "dd";
+            photo.PhotoFile = System.IO.File.ReadAllBytes("./../Images/flower.jpg");
+            photo.ImageMimeType = "image/jpeg";
+            photo.CreatedDate = new DateTime();
+            photos.Add(photo);
+            foreach (var p in photos)
+            {
+                context.Photos.Add(p);
+            }
+            context.SaveChanges();
+            List<Comment> comments = new List<Comment>();
+            Comment comment = new Comment();
+            comment.PhotosID = 1; comment.User = "dd";
+            comment.Subject = "Test Comment";
+            comment.Body = "This comment should appear in photo 1";
+            comments.Add(comment);
+            foreach (var c in comments)
+            {
+                context.Comments.Add(c);
+            }
+            context.SaveChanges();
+            //base.Seed(context);     
 
-
-
-
-
+        }
